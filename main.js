287a0ecb1f7c099352f00e19b42c8a0f69062c19
Sama Mamdouh <samam826@yahoo.com>
2023-10-29 19:37:58 +0200

    Add files via upload
    Add Some Feature

--- main.js
+++ main.js
@@ -1,28 +1,58 @@
-var productNameInput = document.getElementById('productName');
-var productPriceInput=document.getElementById('productPrice');
-var productCategoryInput=document.getElementById('productCategory');
-var productDescInput=document.getElementById('productDesc');
-
-var productCoctainer=[];
-
-function addProduct (){
-  var product={
-    name:productNameInput.value,
-    price:productPriceInput.value,
-    category:productCategoryInput.value,
-    desc:productDescInput.value,
-  }
-
-  productCoctainer.push(product);
-
-  console.log(productCoctainer)
-  clearProduct()
+var productContainer;
+if (localStorage.getItem("products") != null) {
+  productContainer = JSON.parse(localStorage.getItem("products"));
+  displayProduct();
+} else {
+  productContainer = [];
 }
 
-function clearProduct(){
-    productNameInput.value="";
-    productPriceInput.value="";
-    productCategoryInput.value="";
-    productDescInput.value="";
+var productNameInput = document.getElementById("productName");
+var productPriceInput = document.getElementById("productPrice");
+var productCategoryInput = document.getElementById("productCategory");
+var productDescInput = document.getElementById("productDesc");
 
+//var productContainer = [];
+
+function addProduct() {
+  // Main Function
+  var product = {
+    name: productNameInput.value,
+    price: productPriceInput.value,
+    category: productCategoryInput.value,
+    desc: productDescInput.value,
+  };
+
+  productContainer.push(product);
+
+  localStorage.setItem("products", JSON.stringify(productContainer));
+  console.log(productContainer);
+  clearProduct();
+  displayProduct();
+}
+
+function clearProduct() {
+  productNameInput.value = "";
+  productPriceInput.value = "";
+  productCategoryInput.value = "";
+  productDescInput.value = "";
+}
+
+function displayProduct() {
+  var tableHold = ``;
+
+  for (var i = 0; i < productContainer.length; i++) {
+    tableHold += `<tr>
+    <td>${i}</td>
+    <td>${productContainer[i].name}</td>
+    <td>${productContainer[i].price}</td>
+    <td>${productContainer[i].category}</td>
+    <td>${productContainer[i].desc}</td>
+    <td><button type="button" class="btn btn-warning">Update</button>
+    </td>
+    <td><button type="button" class="btn btn-warning">Delete</button>
+    </td>
+    </tr>`;
+  }
+
+  document.getElementById("tableBody").innerHTML = tableHold;
 }
